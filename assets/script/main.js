efd78949e845e058145ec019f8ae13462940e2e6
fabiodelli <ntqdel@hotmail.com>
2023-03-08 15:04:56 +0100

    add human code

---
+++ assets/script/main.js
@@ -0,0 +1,26 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+Questo richiederà un minimo di ricerca.
+ */
+
+//Procedimento
+
+// Chiedo all'utente il numero di chilometri e l'età
+
+/* Calcolo il prezzo base del biglietto:
+   - il prezzo del biglietto è definito in base ai km (0.21 € al km)/*
+
+/* Applico lo sconto in base all'età:
+   - va applicato uno sconto del 20% per i minorenni
+   - va applicato uno sconto del 40% per gli over 65.*/
+
+/*inserisco l'output del prezzo finale nel DOM in forma umana:
+ (con massimo due decimali, per indicare centesimi sul prezzo).*/
+
+
+
+
